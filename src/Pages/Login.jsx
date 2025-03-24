5a8ebda071b4f453c97ab8b43a5af431d9e277c3
RIPUNJAY RAI <111183094+ripunjayrai777@users.noreply.github.com>
2025-03-24 20:45:38 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleLogin = async(e)=>{
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:4000/api/v1/user/login", {email, password, confirmPassword, role: "Patient"}, {withCredentials: true, headers: {"Content-Type": "application/json"}});
+      const response = await axios.post("https://hospital-management-backend-2mf468f5g-ripunjayrai777s-projects.vercel.app//api/v1/user/login", {email, password, confirmPassword, role: "Patient"}, {withCredentials: true, headers: {"Content-Type": "application/json"}});
       toast.success(response.data.message);
       setIsAuthenticated(true);
       navigateTo("/");
